a1580282f8a2f2ffd819253ffb6ce52003373795
Eiichi Takebuchi <nanashi4129@gmail.com>
2012-11-27 16:29:03 +0900

    add drop table

--- init.rb
+++ init.rb
@@ -3,3 +3,20 @@
 <CAUTION>
 drop and initialize sqlite3 tables.
 =end
+
+require 'sqlite3'
+
+def DropTable(db)
+  db.execute("drop table user_table")
+  db.execute("drop table account_table")
+end
+
+def CreateTable(db)
+  db.execute("create table user_table (userid integer primary key, username text);")
+  db.execute("")
+end
+
+db = SQLite3::Database.new("accounts.db")
+  DropTable(db)
+  
+db.close
